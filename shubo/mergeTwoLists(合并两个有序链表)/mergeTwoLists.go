cb28640d057e93d266f368cdd31664a4de8508bb
KennethBo <kenneth@selefra.io>
2023-03-06 20:26:44 +0800

    update: 20230306

--- shubo/mergeTwoLists(合并两个有序链表)/mergeTwoLists.go
+++ shubo/mergeTwoLists(合并两个有序链表)/mergeTwoLists.go
@@ -14,22 +14,19 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	return rescur(list1, list2)
 }
 func rescur(list1 *ListNode, list2 *ListNode) *ListNode {
-	if list1 == nil && list2 == nil {
-		return nil
-	}
 	if list1 == nil {
 		return list2
 	}
 	if list2 == nil {
 		return list1
 	}
-	if list1.Val > list2.Val {
-		cur.Next = rescur(list1, list2.Next)
+	if list1.Val < list2.Val {
+		list1.Next = rescur(list1.Next, list2)
+		return list1
 	} else {
-		cur.Next = rescur(list1.Next, list2)
+		list2.Next = rescur(list1, list2.Next)
+		return list2
 	}
-
-	return &cur
 }
 func main() {
 	v := mergeTwoLists(&ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 5}}},
